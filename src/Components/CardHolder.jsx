013731bea5c690603ad1205867ec33eedc001a11
Suhail Malik <devmsuhail@gmail.com>
2020-10-21 17:39:46 +0530

    Added Throttling

--- src/Components/CardHolder.jsx
+++ src/Components/CardHolder.jsx
@@ -1,19 +1,40 @@
 import React, { useState } from 'react';
 import Card from './Card';
+import './CardHolder.css'
 
-function throttle() {}
+function throttleWork(cb, time) {
+    let throttle = null
+
+    return () => {
+        if (!throttle) {
+            throttle = setTimeout(() => {
+                cb()
+                throttle = null
+                console.log('updated')
+            }, 5000)
+        }
+    }
+}
+
 
 function CardHolder() {
     const [data, setData] = useState(25);
+    const [loading, setLoading] = useState(true)
+
+    const setDataThrottle = throttleWork(() => {
+        setData(prevState => prevState + 25)
+        setLoading(false)
+    }, 3000)
 
     const handleScroll = (e) => {
         if (
             e.target.scrollHeight -
-                e.target.scrollTop -
-                e.target.clientHeight ===
+            e.target.scrollTop -
+            e.target.clientHeight ===
             0
         ) {
-            setData((prevState) => prevState + 25);
+            setLoading(true)
+            setDataThrottle()
         }
     };
 
@@ -37,6 +58,8 @@ function CardHolder() {
                 .map((ele) => (
                     <Card key={ele}>{ele}</Card>
                 ))}
+
+            {loading && <div className='loading' ></div>}
         </div>
     );
 }
